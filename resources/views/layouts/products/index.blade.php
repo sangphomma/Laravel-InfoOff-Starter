fbc7195cee2f33f274ed633302bfae240f43859d
sangphomma <87381359+sangphomma@users.noreply.github.com>
2023-12-19 14:05:02 +0700

    Config public/build*

--- resources/views/layouts/products/index.blade.php
+++ resources/views/layouts/products/index.blade.php
@@ -3,7 +3,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/app.css')
+  {{-- @vite('resources/css/app.css') --}}
+  @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 <body>
     <div class='flex flex-col w-screen min-h-screen ' >
