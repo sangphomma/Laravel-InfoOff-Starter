e8e64a95f4dacec9cc8deff606050b930cedcaca
sangphomma <87381359+sangphomma@users.noreply.github.com>
2023-12-20 08:14:12 +0700

    Create- TodoResource Index & Show recode

---
+++ app/Http/Controllers/api/v1/TodoController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\api\v1;
+
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\TodoResource;
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $todos =Todo::all() ;
+
+        return TodoResource::collection($todos) ;
+    }
+        /**
+     * Display the specified resource.
+     */
+    public function show(Todo $todo)
+    {
+        return TodoResource::make($todo) ;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
